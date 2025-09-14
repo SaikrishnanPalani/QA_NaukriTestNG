dd53e6e63b5b6ba83aeaa751f57ed2e425c44928
sai krishnan Palani <saikrishnanpalani@gmail.com>
2025-09-14 23:59:56 +0530

    Added changes

---
+++ BankAccount/src/account/com/BankAccount.java
@@ -0,0 +1,59 @@
+package account.com;
+
+public class BankAccount {
+
+	public void Info() {
+		
+		String Name = "Sai Krishnan Palani";
+		long Accno= 934577788;
+		
+		
+		System.out.println("Account holder Name:"+ Name);
+		System.out.println("Account number::"+ Accno);
+		
+	}
+	
+	public void Deposit() {
+		
+		int a = 20000;
+		
+		System.out.println("Deposit Amount is:"+ a);
+		
+	}
+	
+	public void Withdraw() {
+		
+		int b = 10000;
+
+		System.out.println("Withdraw Amount is:"+ b);
+				
+	}
+
+	public void Balance() {
+		
+		int a = 20000;
+		int b = 10000;
+		int c = a-b;
+		System.out.println("Balance Amount is:"+ c);
+		
+	}
+	public static void main(String args[]) {
+		BankAccount bank = new BankAccount();
+		bank.Info();
+		bank.Deposit();
+		bank.Withdraw();
+		bank.Balance();
+		
+	}
+		
+		
+		
+
+
+}
+
+
+
+
+
+
